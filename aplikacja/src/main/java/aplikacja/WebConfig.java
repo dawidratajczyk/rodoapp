00899a4854eaed5ab7414eabaa45d26c79e1f830
dratajczyk <dratajczyk@dratajc-nbc-pro.maflow.group>
2020-01-22 15:50:36 +0100

    secoundTry

--- aplikacja/src/main/java/aplikacja/WebConfig.java
+++ aplikacja/src/main/java/aplikacja/WebConfig.java
@@ -1,8 +1,11 @@
 package aplikacja;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,5 +25,23 @@ public class WebConfig implements WebMvcConfigurer {
 	    bean.setSuffix(".jsp");
 	    return bean;
 	}
+	
+	
+	@Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");  
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/baza");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("123456");
+         
+        return dataSource;
+    }
+	
+
+	   @Bean
+	    public dbSlave getContactDAO() {
+	        return new dbControler(getDataSource());
+	    }
 
 }
