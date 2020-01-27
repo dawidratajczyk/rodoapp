83113eeb3beaf8642a74e31856278079ed7ac698
dratajczyk <dratajczyk@dratajc-nbc-pro.maflow.group>
2020-01-27 14:43:47 +0100

    fristCommit

---
+++ MesAplication/src/main/java/MesAplication/MesAplicationApplication.java
@@ -0,0 +1,14 @@
+package MesAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MesAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesAplicationApplication.class, args);
+	}
+
+}
---
+++ MesAplication/src/test/java/MesAplication/MesAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package MesAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
