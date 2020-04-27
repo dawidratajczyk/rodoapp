6680ddace2cb09bf3a364b8694854bc60cf9cda3
dratajczyk <dratajczyk@dratajc-nbc-pro.maflow.group>
2020-04-27 19:38:39 +0200

    Norma nie dziala

--- MesAplication/src/main/resources/static/js/dane.js
+++ MesAplication/src/main/resources/static/js/dane.js
@@ -1,30 +1,35 @@
   $(document).ready(function(){
-	  $('#fragment').on('change', function(){   
-		var kod = this.value;
+	 var URL = 'http://localhost:8080/'
+	  $('#szukaj').on('click', function(){   
+		var kod = fragment.value;
 	 $.ajax({
 		type:'GET',
-		url: 'http://localhost:8080/findrecord?mat=' + kod,	
+		url: URL + 'findrecord?mat=' + kod,
 				success: function(result){
 				//var result = JSON.parse(result);//
-				var s = '';
+				var s = '<option>wybierz kod</option>';
 				for(var i = 0; i < result.length; i++) {
 					s += '<option>' + result[i].material + '</option>';
 					
 				}
 				$('#kod').html(s);
-				
+				$('#operacja').html('<option></option>');
+				$('#czas').html('<option></option>');
+				$('#norma').html('<option></option>');
 				
 			}
 		
 	 })
 	})
 	
+
+	
 	
 		  $('#kod').on('change', function(){   
 		var material = this.value;
 	 $.ajax({
 		type:'GET',
-		url: 'http://localhost:8080/findoperation?mat=' + material,	
+		url: URL + 'findoperation?mat=' + material,	
 				success: function(result){
 				//var result = JSON.parse(result);//
 				var s = '';
@@ -41,8 +46,38 @@
 	})
 	
 	
+	//// Po wyborze kodu oraz operacji w polu czas pojawi sie czas.
+	// poprawic norme
+	  $('#operacja').on('change', function(){   
+		var material = kod.value;
+		var operacja = this.value;
+	 $.ajax({
+		type:'GET',
+		url: URL + 'findtime?mat=' + material +"&op=" + operacja,
+				success: function(result){
+				//var result = JSON.parse(result);//
+				var s = '';
+				for(var i = 0; i < result.length; i++) {
+					s += '<option>' + result[i].wartoscstandardowa2 + '</option>';
+					
+				}
+				$('#czas').html(s);
+				//var n = 60/(s.replace(",","."));
+				var n = parseFloat(s.replace(",","."));
+				var w = (n / 60);
+				console.log(w);
+				$('#norma').html(w);
+
+				
+				
+			}
+		
+	 })
+	})
 	
+
 	
+
 	
 	
 	
