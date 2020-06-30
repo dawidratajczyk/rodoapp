869aa4534579a77d7a09b353e160fac213b8b3be
dratajczyk <dratajczyk@dratajc-nbc-pro.maflow.group>
2020-06-30 11:42:12 +0200

    RODO first Commit

---
+++ rodo/src/main/java/pl/mmstomatologia/RodoApplication.java
@@ -0,0 +1,13 @@
+package pl.mmstomatologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodoApplication.class, args);
+	}
+
+}
