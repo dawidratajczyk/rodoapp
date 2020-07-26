738c6cc3b622707ec919ab28fa11d28263c72ed5
dawidratajczyk <59934287+dawidratajczyk@users.noreply.github.com>
2020-07-26 20:33:57 +0200

    CleaningTestFiles

--- MesAplication/src/main/java/MesAplication/resControler/crudControlerDlaMaszyn.java
+++
@@ -1,9 +0,0 @@
-package MesAplication.resControler;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import MesAplication.resControler.entities.maszyny;
-
-public interface crudControlerDlaMaszyn extends JpaRepository<maszyny, Long>{
-
-}
